6e5eb51c6cdb0993292f2f7234ad1d505305f5f0
ribao wei <wrb302@gmail.com>
2014-02-08 13:19:47 -0800

    moved test to a separate file and removed unnecessary code in onewayBinding()

--- test.js
+++ test.js
@@ -1,79 +1,10 @@
 var Backbone = require("backbone");
+var zerg = require("./main");
 var assert = require("assert");
 
-var binding = function(model1, field1, model2, field2){
-  if(field2===undefined){
-    field2 = field1;
-  }
-  oneWayBinding(model1, field1, model2, field2);
-  oneWayBinding(model2, field2, model1, field1);
-
-}
-
-var oneWayBinding = function(model1, field1, model2, field2){
-  if(field2===undefined){
-    field2 = field1;
-  }
-  model2.set(field2, model1.get(field1));
-
-  model1.on("change:"+field1, function(options){
-    model1.trigger("propagate:"+field1, {
-      visited: {model1: true}
-    });
-  });
-
-  model2.listenTo(model1, "propagate:"+field1, function(options){
-    var visited = options.visited;
-    if(!visited[model2.cid]){
-      model2.set(field2, model1.get(field1));
-      visited[model2.cid] = true;
-      model2.trigger("propagate:"+field1, {
-        visited: visited
-      });
-    }
-  });
-
-}
-
-var computedProperty = function(model, field, deps, fn){
-  function onDependencyChanged(m, v, options){
-    var resovledDeps = deps.map(function(dep){
-      var depModel, depField;
-      if(Array.isArray(dep)){
-        depModel = dep[0];
-        depField = dep[1];
-      } else {
-        depModel = model;
-        depField = dep;
-      }
-      return depModel.get(depField);
-    });
-
-    options = options || {};
-    var visited = options.visited || {};
-    if(visited[model.cid] === true){
-      throw new Error("Cycular dependencies detected!");
-    };
-    visited[model.cid] = true;
-    model.set(field, fn.apply(null, resovledDeps), {
-      visited: visited
-    });
-  }
-
-  deps.forEach(function(dep){
-    var depModel, depField;
-    if(Array.isArray(dep)){
-      depModel = dep[0];
-      depField = dep[1];
-    } else {
-      depModel = model;
-      depField = dep;
-    }
-    model.listenTo(depModel, "change:" + depField, onDependencyChanged);
-    onDependencyChanged();
-  });
-
-}
+var binding = zerg.binding;
+var onewayBinding = zerg.onewayBinding;
+var computedProperty = zerg.computedProperty;
 
 // tests
 
